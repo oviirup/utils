ec775dca686e5d783d41a77f561fe86aebe371b6
oviirup <49820575+oviirup@users.noreply.github.com>
2025-12-08 23:11:10 +0530

    ♻️ added `pick` and `omit` to object

--- src/object.ts
+++ src/object.ts
@@ -1,3 +1,5 @@
+import { toArray } from "./array";
+
 type Dict<T = unknown> = Record<string, T>;
 
 /**
@@ -25,4 +27,34 @@ export function keyInObject<T extends object = Dict>(
   key: keyof T | (string & {}),
 ): boolean {
   return isObject(val) && key in val;
+}
+
+/**
+ * Picks a set of keys from an object
+ * @category Object
+ */
+export function pick<T extends object, K extends keyof T = keyof T>(
+  input: T,
+  keys: K | K[],
+): Pick<T, K> {
+  const picked = {} as Pick<T, K>;
+  for (const key of toArray(keys)) {
+    if (key in input) picked[key] = input[key];
+  }
+  return picked;
+}
+
+/**
+ * Omits a set of keys from an object
+ * @category Object
+ */
+export function omit<T extends object, K extends keyof T = keyof T>(
+  input: T,
+  keys: K | K[],
+): Omit<T, K> {
+  const partial = { ...input } as T;
+  for (const key of toArray(keys)) {
+    if (key in input) delete partial[key];
+  }
+  return partial as Omit<T, K>;
 }
