77aa1ccd633dd04983f8f90c7ea86ad8eba13e36
oviirup <49820575+oviirup@users.noreply.github.com>
2026-04-08 14:55:30 +0530

    🗑️ removed clsx

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * as array from "./array";
 export * from "./assertions";
-export * from "./clsx";
 export * from "./nanoid";
 export * as number from "./number";
 export * as object from "./object";
